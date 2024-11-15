05d5da8434cc3bd7120b3e8a583d97a91c8f1c84
Zinaida Bizgan <zinaida.bizgan@smartsoftdev.eu>
2024-11-15 23:14:22 +0200

    Added the DriverManager implementation and related libraries in POM.xml

--- src/main/java/md/tekwill/TestRunner.java
+++ src/main/java/md/tekwill/TestRunner.java
@@ -1,7 +1,29 @@
 package md.tekwill;
 
+import md.tekwill.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://www.google.com/");
+
+        String theNameOfTheFirstTab = driver.getWindowHandle();
+
+        // Open a new window and navigate to the Diez page
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://diez.md/");
+        Thread.sleep(2000);
+        // Close the current tab
+        driver.close();
+
+        driver.switchTo().window(theNameOfTheFirstTab);
+        driver.get("https://www.stiri.md/");
+        Thread.sleep(2000);
+        driver.close();
+
+
     }
 }
--- src/main/java/md/tekwill/managers/ExampleManager.java
+++
@@ -1,4 +0,0 @@
-package md.tekwill.managers;
-
-public class ExampleManager {
-}
